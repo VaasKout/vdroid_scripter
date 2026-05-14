acbc52feb563e74fa1981e3a0a422d9dc3066bef
VaasKout <VasyaKotov1@yandex.ru>
2026-05-14 08:45:29 -0400

    fix keyboard menu item, send locale and fix typing

---
+++ server/pkg/core/strutils/str_utils.go
@@ -0,0 +1,19 @@
+// Package strutils ...
+package strutils
+
+func GetUniqueChars(text string) []rune {
+	if text == "" {
+		return []rune{}
+	}
+
+	uniqueChars := map[rune]struct{}{}
+	for _, ch := range text {
+		uniqueChars[ch] = struct{}{}
+	}
+	filteredRunes := make([]rune, 0, len(uniqueChars))
+
+	for k := range uniqueChars {
+		filteredRunes = append(filteredRunes, k)
+	}
+	return filteredRunes
+}
