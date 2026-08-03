4e38d45ed1909e9ab56ba2ba424fb7064709cda1
VaasKout <VasyaKotov1@yandex.ru>
2026-08-04 00:34:41 +0300

    remove redundant code

--- android_client/data/api/src/main/java/com/vision/scripter/data/api/models/Scripts.kt
+++ android_client/data/api/src/main/java/com/vision/scripter/data/api/models/Scripts.kt
@@ -1,7 +1,5 @@
 package com.vision.scripter.data.api.models
 
-import android.view.MotionEvent.ACTION_DOWN
-import android.view.MotionEvent.ACTION_UP
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -42,24 +40,6 @@ data class Event(
     @SerialName("data")
     val data: ByteArray? = null,
 )
-
-fun List<Event>.extractPressEvent(): List<Event> {
-    val pressAction = mutableListOf<Event>()
-    forEachIndexed { index, event ->
-        val data = event.data ?: return@forEachIndexed
-        val nextData = if (index < this.size - 1) this[index + 1].data else null
-
-        if (
-            index < this.size - 1 && data.size == 32 && data[1].toInt() == ACTION_DOWN &&
-            nextData != null && nextData[1].toInt() == ACTION_UP
-        ) {
-            pressAction.add(this[index])
-            pressAction.add(this[index + 1])
-            return pressAction
-        }
-    }
-    return pressAction
-}
 
 @Serializable
 data class KeyboardButtons(
